91e7a3679880a1cffb433f7c6ec617a8e0a6cc5b
NgoDinhHoangHa <hoanghadn0123@gmail.com>
2022-08-27 15:35:42 +0700

    tuan1

---
+++ Tuan1/Demo.rb
@@ -0,0 +1,15 @@
+name = "Hoàng Hà"
+msv = "1911505310213"
+puts "Xin chào #{name} #{msv}"
+print "Nhap 1 so: "
+number = gets
+puts "Số vừa nhập là: #{number}"
+print "Nhập gia trị thứ 1: "
+nb1 = gets
+print "Nhập giá trị thứ 2: "
+nb2 = gets
+if nb1 > nb2
+    puts "#{nb1}"
+else 
+    puts "#{nb2}"
+end
