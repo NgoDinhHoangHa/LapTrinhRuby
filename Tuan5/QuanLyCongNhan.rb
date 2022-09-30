fcf8879ae36527673e6a4846321568549d9d0724
NgoDinhHoangHa <hoanghadn0123@gmail.com>
2022-09-30 10:54:03 +0700

    fix

--- Tuan5/QuanLyCongNhan.rb
+++ Tuan5/QuanLyCongNhan.rb
@@ -1,88 +1,96 @@
 class Officer
-    attr_accessor :name, :age, :gender, :address
-    def initialize name, age,gender,address
-        @name = name
-        @age = age
-        @gender = gender
-        @address = @address
+    attr_accessor :name213, :age213, :gender213, :address213
+    def initialize name213, age213,gender213,address213
+        @name213 = name213
+        @age213 = age213
+        @gender213 = gender213
+        @address213 = @address213
     end
     def display
-        puts ("Ten: #{@name}")
-        puts ("Tuoi: #{@age}")
-        puts ("Gioi Tinh: #{@gender}")
-        puts ("Dia Chi:  #{@address}")
+        puts ("Ten: #{@name213}")
+        puts ("Tuoi: #{@age213}")
+        puts ("Gioi Tinh: #{@gender213}")
+        puts ("Dia Chi:  #{@address213}")
     end
 end
 class Staff < Officer
-    attr_accessor :task
-    def initialize name, age,gender,address, task
-        super(name,age,gender,address)
-        @task = task
+    attr_accessor :task213
+    def initialize name213, age213,gender213,address213, task213
+        super(name213,age213,gender213,address213)
+        @task213 = task213
     end
     def display
         super()
-        puts ("Cong vien duoc giao: #{@task}")
+        puts ("Cong vien duoc giao: #{@task213}")
     end
 end
 class Engineer < Officer
-    attr_accessor :branch
-    def initialize name, age,gender,address,branch
-        super(name, age,gender,address)
-        @branch = branch
+    attr_accessor :branch213
+    def initialize name213, age213,gender213,address213,branch213
+        super(name213, age213,gender213,address213)
+        @branch213 = branch213
     end
     def display
         super()
-        puts ("Nganh dao tao: #{@branch}")
+        puts ("Nganh dao tao: #{@branch213}")
     end
 end
 class Worker < Officer
-    attr_accessor :level
-    def initialize name,age,gender,address,level
-        super(name,age,gender,address)
-        @level = level
+    attr_accessor :level213
+    def initialize name213,age213,gender213,address213,level213
+        super(name213,age213,gender213,address213)
+        @level213 = level213
     end
     def display
         super()
-        puts ("Bac Luong: #{@level}")
+        puts ("Bac Luong: #{@level213}")
     end
 end
 
 class ManagerOfficer
-    attr_accessor :ds
+    attr_accessor :ds213
     def initialize 
-        @ds = Array.new
+        @ds213 = Array.new
     end
-    def Add name,age,gender,address
-        of = Officer.new(name,age,gender,address)
-        @ds.push(of)
+    def Add name213,age213,gender213,address213
+        of213 = Officer.new(name213,age213,gender213,address213)
+        @ds213.push(of213)
     end
-
+    def searchByName name213
+        lenght213 = @ds213.length
+        for i in 0..lenght do
+            @ds213[i].display
+        
+    end
+    end
 end
 class Main
-    dem=0
-    mo=ManagerOfficer.new()
-    until dem==1 do
+    dem213=0
+    mo213=ManagerOfficer.new()
+    until dem213==1 do
        puts "1. them"
        puts "2. tim kiem"
        puts "3. thoat"
-       dem=gets
-       if dem.to_i ==3
+       dem213=gets
+       if dem213.to_i ==3
             exit
-       elseif dem.to_i == 1
+        end
+        if dem213.to_i == 1
             print " Nhap ten :"
-            name=gets
+            name213=gets
             print "nhap tuoi: "
-            age=gets
+            age213=gets
             print "gioi tinh: "
-            gender =gets
+            gender213 =gets
             print "dia chi: "
-            address=gets
-            mo.Add(name,age,gender,address)
-       if test
-        
-       else
-        
-       end
-end
+            address213=gets
+            mo213.Add(name213,age213,gender213,address213)
+        end
+        if dem213.to_i == 2
+            print "Nhap ten: "
+            ten213 = gets
+            mo213.searchByName(ten213)
+        end
+    end
 end
 
